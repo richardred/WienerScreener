f37c5df94f9f01c4664c36b79164043ca40bf5dd
Richard Red <richardred.q@gmail.com>
2018-02-14 16:25:14 -0500

    Update run-webserver.sh

--- server/run-webserver.sh
+++ server/run-webserver.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+#This script runs the web server on Docker using Caddy for secure, automatic HTTPS certificates
+
 docker run -it --rm -v $(pwd)/Caddyfile:/etc/Caddyfile -v $HOME/.caddy:/root/.caddy -v $(pwd)/web:/web -p 80:80 -p 443:443 abiosoft/caddy
