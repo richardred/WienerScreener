228b1f954ee40a4ea2625f880fea980b17c23c57
Richard Red <richardred.q@gmail.com>
2018-02-13 22:48:22 -0500

    Update random-wiener.sh

--- server/wiener/random-wiener.sh
+++ server/wiener/random-wiener.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#This script finds a random picture in the directory by shuffling the files and then selecting the first one.
 
 file=$(find tf_files/data/wiener_images/ | shuf | head -n 1)
 echo "Analyzing $file"
